263c11f3973904e6c151a6642a36bcd4c6f09a81
altyntsevlexus <point-alex10@ukr.net>
2021-10-25 19:51:04 +0300

    feat(api): getBlocks api added

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const initialURL = (network) =>
+  `https://api.teztracker.com/v2/data/tezos/${network}`;
+
+const getBlocks = (network = 'mainnet', offset = '0', limit = '15') =>
+  axios.get(`${initialURL(network)}/blocks?limit=${limit}&offset=${offset}`);
+
+export default getBlocks;
