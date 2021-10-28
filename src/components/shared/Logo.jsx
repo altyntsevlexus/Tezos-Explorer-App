f92e1d49b65dcff00c63d55a29b63cd6db0739cb
altyntsevlexus <point-alex10@ukr.net>
2021-10-28 13:03:09 +0300

    fix: comment removed

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,8 +1,5 @@
 import { Link } from 'react-router-dom';
 
-const Logo = () => (
-  // eslint-disable-next-line jsx-a11y/anchor-is-valid
-  <Link to="/blocks">Logo</Link>
-);
+const Logo = () => <Link to="/blocks">Logo</Link>;
 
 export default Logo;
