40e124e6ba75acbd0f78090e7db2bec2851cb5bd
altyntsevlexus <point-alex10@ukr.net>
2021-11-12 13:01:56 +0200

    fix: copyright - one file / positioning by parent

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,12 +1,18 @@
 import styles from './_Footer.module.scss';
 import Contacts from '../Contacts';
 import Copy from '../Copy';
+import Logo from '../Logo';
 
 const Footer = () => (
   <footer className={styles.footer}>
     <div className="wrapper">
       <Contacts />
-      <Copy />
+      <div className={styles.footer__copy}>
+        <Copy />
+        <div className={styles.footer__logo}>
+          <Logo />
+        </div>
+      </div>
     </div>
   </footer>
 );
