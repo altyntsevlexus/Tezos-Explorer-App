2cc743822b9409da5194674b6288b56592070971
Alexandr-Petrenko <alexandr.petrenko.fe@gmail.com>
2021-11-15 14:58:50 +0200

    fix(api): limit and offset type was changed to number

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const tezTrakerAPI = axios.create({
   baseURL: 'https://api.teztracker.com/v2/data/tezos/',
 });
 
-const getBlocks = (network = 'mainnet', offset = '0', limit = '15') =>
+const getBlocks = (network = 'mainnet', offset = 0, limit = 15) =>
   tezTrakerAPI.get(`${network}/blocks?limit=${limit}&offset=${offset}`);
 
 const getBlock = (network = 'mainnet', id) =>
