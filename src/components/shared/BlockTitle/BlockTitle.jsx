6d8e110c2128840a00563dfc64d6a0674534d9ce
altyntsevlexus <point-alex10@ukr.net>
2021-11-15 20:44:22 +0200

    fix: styles renamed a bit accroding to BEM

--- src/components/shared/BlockTitle/BlockTitle.jsx
+++ src/components/shared/BlockTitle/BlockTitle.jsx
@@ -16,10 +16,10 @@ const BlockTitle = ({ className }) => {
   return (
     <div className={`${styles['block-title']} ${className}`}>
       {total === level ? (
-        <span className={styles['block-title__items']}>{'<'}</span>
+        <span className={styles['block-title__item']}>{'<'}</span>
       ) : (
         <Link
-          className={styles['block-title__items']}
+          className={styles['block-title__item']}
           aria-label="Next block id"
           to={`/blocks/${level + 1}`}
         >
@@ -29,12 +29,12 @@ const BlockTitle = ({ className }) => {
       <div className={styles['block-title__wrapper']}>
         <Title
           value={`Block: ${title}`}
-          className={styles['block-title__title']}
+          className={styles['block-title__heading']}
         />
         <h3 className={styles['block-title__sub-title']}>Block information</h3>
       </div>
       <Link
-        className={styles['block-title__items']}
+        className={styles['block-title__item']}
         role="button"
         aria-label="Previous block id"
         to={`/blocks/${level - 1}`}
