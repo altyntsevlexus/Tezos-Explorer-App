9da86c797f0f326ad4e75f1b6b9fc5d9a387fd4e
altyntsevlexus <point-alex10@ukr.net>
2021-11-16 12:16:30 +0200

    fix: instance renamed

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const tezTrakerAPI = axios.create({
+const tezTrackerAPI = axios.create({
   baseURL: 'https://api.teztracker.com/v2/data/tezos/',
 });
 
 const getBlocks = (network = 'mainnet', offset = 0, limit = 15) =>
-  tezTrakerAPI.get(`${network}/blocks?limit=${limit}&offset=${offset}`);
+  tezTrackerAPI.get(`${network}/blocks?limit=${limit}&offset=${offset}`);
 
 const getBlock = (network = 'mainnet', id) =>
-  tezTrakerAPI.get(`${network}/blocks/${id}`);
+  tezTrackerAPI.get(`${network}/blocks/${id}`);
 
 const getHead = (network = 'mainnet') =>
-  tezTrakerAPI.get(`${network}/blocks/head`);
+  tezTrackerAPI.get(`${network}/blocks/head`);
 
 export { getBlocks, getBlock, getHead };
