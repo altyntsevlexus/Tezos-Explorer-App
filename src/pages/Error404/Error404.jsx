280750234903a013266a1b9f5fc92846fe0e19f9
altyntsevlexus <point-alex10@ukr.net>
2021-11-23 02:23:45 +0200

    feat: 404 page styled

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import styles from './_Error404.module.scss';
 
 const Error404 = () => (
-  <div className="error">
-    <h1>404</h1>
-    <span>Sorry, the page you’re looking for can’t be found</span>
-    <Link to="/blocks" className="error__button">
-      Blocks
+  <div className={styles.error}>
+    <h1 className={`wrapper__title ${styles.error__code}`}>404</h1>
+    <p className={styles.error__text}>
+      Sorry, the page you’re looking for can’t be found
+    </p>
+    <Link to="/blocks" className={styles.error__button}>
+      Home page
     </Link>
   </div>
 );
