5fd5ee85e32291ed9e1564d537fdd381e29dda35
altyntsevlexus <point-alex10@ukr.net>
2021-11-24 02:21:30 +0200

    feat: API dynamic network

--- src/contexts/networkContext.jsx
+++ src/contexts/networkContext.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState, useEffect, useMemo } from 'react';
 
 const NetworkStateContext = createContext('');
 NetworkStateContext.displayName = 'Network Context';
@@ -15,11 +15,44 @@ const useNetworkState = () => {
   return context;
 };
 
-const NetworkProvider = ({ children }) => (
-  <NetworkStateContext.Provider value="mainnet">
-    {children}
-  </NetworkStateContext.Provider>
-);
+const NetworkProvider = ({ children }) => {
+  const [network, setNetwork] = useState('mainnet');
+
+  const storage = sessionStorage;
+
+  const handleSetNetwork = (newNetwork) => {
+    setNetwork(newNetwork);
+    storage.setItem('network', newNetwork);
+  };
+
+  useEffect(() => {
+    switch (storage.getItem('network')) {
+      case 'mainnet':
+        setNetwork('mainnet');
+        break;
+      case 'another':
+        setNetwork('another');
+        break;
+      default:
+        setNetwork('mainnet');
+        storage.setItem('network', 'mainnet');
+    }
+  }, []);
+
+  const stateValue = useMemo(
+    () => ({
+      network,
+      handleSetNetwork,
+    }),
+    [network, handleSetNetwork],
+  );
+
+  return (
+    <NetworkStateContext.Provider value={stateValue}>
+      {children}
+    </NetworkStateContext.Provider>
+  );
+};
 
 NetworkProvider.propTypes = {
   children: PropTypes.node.isRequired,
