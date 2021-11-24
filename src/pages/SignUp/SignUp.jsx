eb4c02bac397fb6b45e52142c1664f92fa8fe13c
altyntsevlexus <point-alex10@ukr.net>
2021-11-25 00:32:58 +0200

    fix: Title changed on Login and SignUp

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,8 +4,12 @@ import styles from './_SignUp.module.scss';
 
 const SignUp = () => (
   <section className={styles.login}>
-    <Title value="Sign up" className={styles.login__title} />
-    <p className={styles['login__sub-title']}>Get Started by Signing Up Now</p>
+    <Title
+      value="Sign up"
+      subtitle="Get Started by Signing Up Now"
+      wrapped
+      centered
+    />
     <Form value="signup" />
   </section>
 );
