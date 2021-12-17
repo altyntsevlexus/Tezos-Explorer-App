f0e06cd443f8f6def209364c5568badf907ed9b2
Alexandr-Petrenko <alexandr.petrenko.fe@gmail.com>
2021-12-17 11:54:39 +0200

    fix(validation): validation fields were divided

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,26 +1,28 @@
 import * as Yup from 'yup';
 
+const email = Yup.string()
+  .required('Required')
+  .matches('@', 'Email address must contain the @ character')
+  .email('Invalid email');
+const password = Yup.string()
+  .required(
+    'Password should contain both letter and number, with minimum length of 8 characters ',
+  )
+  .min(8, 'Password minimum length 8 characters')
+  .matches(/[0-9]/, 'Password require at list 1 number 0-9')
+  .matches(/[a-z]/, 'Password must contain at least 1 lowercase letter')
+  .matches(/[A-Z]/, 'Password must contain at least 1 uppercase letter')
+  .matches(/[!@#$%^&*]/, 'Password must contain at least 1 special character');
+const confirm = Yup.string()
+  .required('Required')
+  .oneOf([Yup.ref('password')], 'Passwords do not match');
+const checkbox = Yup.boolean().oneOf([true], 'Required');
+
 const validation = Yup.object({
-  email: Yup.string()
-    .required('Required')
-    .matches('@', 'Email address must contain the @ character')
-    .email('Invalid email'),
-  password: Yup.string()
-    .required(
-      'Password should contain both letter and number, with minimum length of 8 characters ',
-    )
-    .min(8, 'Password minimum length 8 characters')
-    .matches(/[0-9]/, 'Password require at list 1 number 0-9')
-    .matches(/[a-z]/, 'Password must contain at least 1 lowercase letter')
-    .matches(/[A-Z]/, 'Password must contain at least 1 uppercase letter')
-    .matches(
-      /[!@#$%^&*]/,
-      'Password must contain at least 1 special character',
-    ),
-  confirm: Yup.string()
-    .required('Required')
-    .oneOf([Yup.ref('password')], 'Passwords do not match'),
-  checkbox: Yup.boolean().oneOf([true], 'Required'),
+  email,
+  password,
+  confirm,
+  checkbox,
 });
 
 export default validation;
