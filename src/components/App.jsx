99ccad22081aa86070c74ebe20154185465106b2
altyntsevlexus <point-alex10@ukr.net>
2021-12-27 14:22:34 +0200

    feat: Browser router to hash Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import '../styles/index.scss';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRouter from '../route/AppRouter';
